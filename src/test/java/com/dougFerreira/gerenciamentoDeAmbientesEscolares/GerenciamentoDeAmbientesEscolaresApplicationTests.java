ee3840b532bf3159fe882b1fbe098d40ef09909e
Douglas Ferreira da Silva <doiug.ferreira.dfs@gmail.com>
2023-09-05 14:40:41 -0300

    Primeiro commit

---
+++ src/test/java/com/dougFerreira/gerenciamentoDeAmbientesEscolares/GerenciamentoDeAmbientesEscolaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougFerreira.gerenciamentoDeAmbientesEscolares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeAmbientesEscolaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
